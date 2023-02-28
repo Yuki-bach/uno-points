07f13ccd0ec197f4eb93cf51cc61a3e3f6840892
Yuki-bach <yuki20010221@icloud.com>
2023-02-28 14:13:10 +0900

    途中

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,4 +1,23 @@
 class PlayersController < ApplicationController
+  def show
+    @player = Player.find(params[:id])
   def new
+    @player = Player.new
   end
+  def create
+    @player = Player.new(player_params)
+    if @player.save
+      redirect_to @player
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+  def reset
+    @player = Player.update_all(:point => 0)
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'players/new'
   root 'pages#hello'
   get '/home', to: 'pages#home'
   get '/playing', to: 'pages#playing'
   get '/result', to: 'pages#result'
+  # resources: players
 end
