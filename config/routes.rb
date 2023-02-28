52cfbfa5b662220ab3f13c294d5b4626a7b2ef6d
Yuki-bach <yuki20010221@icloud.com>
2023-02-28 12:28:02 +0900

    fix layouts and minimize code of each page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
+  root "pages#hello"
   get 'pages/home'
   get 'pages/playing'
   get 'pages/result'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root "application#hello"
 end
