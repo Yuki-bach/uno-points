2bc10a2a713af78dca2014464755343375b5eeb6
Yuki-bach <yuki20010221@icloud.com>
2023-03-04 14:30:18 +0900

    delete unnecessary files

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,16 @@ Rails.application.routes.draw do
   root 'players#new'
 
   get '/new', to: 'players#new'
-  get '/index', to: 'players#index'
   get '/edit_multiple', to: 'players#edit_multiple'
-  post '/edit_multiple', to: 'players#edit_multiple'
   get 'players/destroy_all', to: 'players#destroy_all'
+
+  post '/edit_multiple', to: 'players#edit_multiple'
   delete 'players', to: 'players#destroy_all'
 
   resources :players, only: [:create, :new, :edit, :update, :destroy_all, :edit_multiple, :update_multiple]
   resources :players do
     collection do
       delete :destroy_all
-      # post 'edit_multiple'
       post 'update_multiple'
     end
   end
