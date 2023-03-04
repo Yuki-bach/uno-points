9c6a5004c42c5bc1cb3e90186906a0f85ae34521
Yuki-bach <yuki20010221@icloud.com>
2023-03-04 12:52:07 +0900

    complete destroy_all in edit_multiple

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,16 @@ Rails.application.routes.draw do
 
   get '/new', to: 'players#new'
   get '/index', to: 'players#index'
-  post '/edit_multiple', to: 'players#edit_multiple'
   get '/edit_multiple', to: 'players#edit_multiple'
-
+  post '/edit_multiple', to: 'players#edit_multiple'
+  get 'players/destroy_all', to: 'players#destroy_all'
+  delete 'players', to: 'players#destroy_all'
 
   resources :players, only: [:create, :new, :edit, :update, :destroy_all, :edit_multiple, :update_multiple]
   resources :players do
     collection do
       delete :destroy_all
-      post 'edit_multiple'
+      # post 'edit_multiple'
       post 'update_multiple'
     end
   end
